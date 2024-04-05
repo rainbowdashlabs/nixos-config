30fecd0247f1546d6c1ccf020b3b19aa6ddc9899
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-04-05 23:40:11 +0200

    Update git settings

--- src/configuration.nix
+++ src/configuration.nix
@@ -174,8 +174,33 @@
           key = null; # Let gpg decide
           signByDefault = true;
         };
+
         userName = "Lilly";
         userEmail = "46890129+RainbowDashLabs@users.noreply.github.com";
+
+        extraConfig = {
+          core = {
+            editor = "nano";
+            autocrlf = "input";
+          };
+
+          checkout = {
+            defaultRemote = "true";
+          };
+
+          pull = {
+            rebase = true;
+          };
+
+          push = {
+            default = "current";
+            autosetupremote = true;
+          };
+
+          init = {
+            defaultbranch = "main";
+          };
+        };
       };
 
 #      gradle = {

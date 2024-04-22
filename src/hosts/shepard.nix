87ca75bbd5d6697871c911418f60d4fbd3a966c2
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-04-22 15:22:30 +0200

    Get rid of nouveau

--- src/hosts/shepard.nix
+++ src/hosts/shepard.nix
@@ -5,6 +5,7 @@
     [ # Include the results of the hosts.hardware scan.
       ./hardware/shepard.nix
       ./../modules
+      ./../modules/graphics/nvidia.nix
     ];
 
   networking.hostName = "shepard";
---
+++ src/modules/graphics/nvidia.nix
@@ -0,0 +1,22 @@
+ { config, lib, pkgs, ... }:
+{
+  boot.blacklistedKernelModules = ["nouveau"];
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
